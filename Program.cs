048214d0308e5911556e819b353a6c24afdd1810
MaKarastY <71918286+makarasty@users.noreply.github.com>
2024-03-18 11:49:10 +0200

    add base

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace LargeFileCreatorNamespace;
+
+class Program
+{
+	static async Task Main()
+	{
+		string filePath = "ShadowOfInsight.txt";
+
+		/*
+		 * 1000000000 lines
+		 * 1000 min
+		 * 100000 max
+		 * ~2min ~7Gb
+		 */
+		int numberOfLines = MyInputV2Int("Кількість рядків: ");
+		int minValue = MyInputV2Int("Мінімальне значення: ");
+		int maxValue = MyInputV2Int("Максимальне значення: ");
+
+		Console.WriteLine("Створюємо файл...");
+
+		try
+		{
+			await WriteRandomNumbersToFileAsync(filePath, numberOfLines, minValue, maxValue + 1);
+			Console.WriteLine($"Файл ({filePath}) успішно записаний!");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Помилка при записі файлу: {ex.Message}");
+		}
+	}
+
+	private static async Task WriteRandomNumbersToFileAsync(string filePath, int numberOfLines, int minValue, int maxValue)
+	{
+		await using var writer = new StreamWriter(filePath);
+
+		Random random = new();
+
+		for (int i = 0; i < numberOfLines; i++)
+		{
+			int randomNumber = random.Next(minValue, maxValue);
+			await writer.WriteLineAsync(randomNumber.ToString());
+		}
+	}
+
+	private static int MyInputV2Int(string text)
+	{
+		while (true)
+		{
+			Console.Write(text);
+			if (int.TryParse(Console.ReadLine(), out int result))
+			{
+				return result;
+			}
+			else
+			{
+				Console.WriteLine("Невірний формат числа. Спробуйте ще раз.");
+			}
+		}
+	}
+}
